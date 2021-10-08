d2847be7005d5c760c147b983c1452f9aa77abaa
LUKE-bit <57066589+LUKE-bit@users.noreply.github.com>
2021-10-08 22:02:24 +0800

    牛顿迭代法 （二分）

---
+++ 有效的完全平方数.java
@@ -0,0 +1,16 @@
+class Solution {
+  public boolean isPerfectSquare(int num) {
+    if (num < 2) return true;
+
+    long x = num / 2;
+    while (x * x > num) {
+      x = (x + num / x) / 2;
+    }
+    return (x * x == num);
+  }
+}
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/valid-perfect-square/solution/you-xiao-de-wan-quan-ping-fang-shu-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
