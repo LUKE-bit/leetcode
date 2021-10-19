1508d0f5365952b970d8546ae365478a66b41344
LUKE-bit <57066589+LUKE-bit@users.noreply.github.com>
2021-10-19 20:37:46 +0800

    Hashmap

---
+++ 有效的字母异位词.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i = 0;i < s.length();i++) {
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0) + 1);
+        }
+        for(int i = 0;i < t.length();i++) {
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0) - 1);
+            if(map.get(t.charAt(i)) < 0) {
+                return false;
+            }
+        }
+        return true;  
+    }
+}
