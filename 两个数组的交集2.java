a94d3276afadbe599514b808b0b78312b10de163
LUKE-bit <57066589+LUKE-bit@users.noreply.github.com>
2021-10-21 21:41:20 +0800

    HashMap

---
+++ 两个数组的交集2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1.length < nums2.length) {
+            return intersect(nums2,nums1);
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i < nums1.length;i++) {
+            map.put(nums1[i],map.getOrDefault(nums1[i],0) + 1);
+        }
+        int[] ans = new int[nums2.length];
+        int index = 0;
+        for(int i = 0;i < nums2.length;i++) {
+            if(map.get(nums2[i]) != null) {
+                map.put(nums2[i],map.getOrDefault(nums2[i],0) - 1);
+                int count = map.get(nums2[i]);
+                if(count >= 0) {
+                    ans[index++] = nums2[i];
+                }
+            }
+        }
+        return Arrays.copyOfRange(ans,0,index);
+    }
+}
