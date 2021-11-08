b3f647315dd1f99542e37feb75f1fef0fe7db9be
LUKE-bit <57066589+LUKE-bit@users.noreply.github.com>
2021-11-08 21:54:14 +0800

    二叉树，深度广度优先遍历

---
+++ 二叉树的所有路径.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        constructPaths(root,"",ans);
+        return ans;
+    }
+    public void constructPaths(TreeNode node,String path,List<String> ans) {
+        if(node != null) {
+            StringBuilder sb = new StringBuilder(path);
+            sb.append(Integer.toString(node.val));
+            if(node.left == null && node.right == null) {
+                ans.add(sb.toString());
+            }else {
+                sb.append("->");
+                constructPaths(node.left,sb.toString(),ans);
+                constructPaths(node.right,sb.toString(),ans);
+            }
+        }
+    }
+}
+
+
+//广度
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<String>();
+        if (root == null) {
+            return paths;
+        }
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        Queue<String> pathQueue = new LinkedList<String>();
+
+        nodeQueue.offer(root);
+        pathQueue.offer(Integer.toString(root.val));
+
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.poll(); 
+            String path = pathQueue.poll();
+
+            if (node.left == null && node.right == null) {
+                paths.add(path);
+            } else {
+                if (node.left != null) {
+                    nodeQueue.offer(node.left);
+                    pathQueue.offer(new StringBuffer(path).append("->").append(node.left.val).toString());
+                }
+
+                if (node.right != null) {
+                    nodeQueue.offer(node.right);
+                    pathQueue.offer(new StringBuffer(path).append("->").append(node.right.val).toString());
+                }
+            }
+        }
+        return paths;
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/binary-tree-paths/solution/er-cha-shu-de-suo-you-lu-jing-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
