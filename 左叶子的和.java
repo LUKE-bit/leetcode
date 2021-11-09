054798687063c4a1b910ce3246cff4fdaa70b501
LUKE-bit <57066589+LUKE-bit@users.noreply.github.com>
2021-11-09 20:01:21 +0800

    二叉树，递归和层序遍历都可以

---
+++ 左叶子的和.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        int count = 0;
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if(node.left != null && node.left.left == null && node.left.right == null) {
+                count+=node.left.val;
+            }
+            if(node.left != null) {
+                stack.push(node.left);
+            }
+            if(node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return count;
+    }
+}
+
+//递归
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        int leftValue = sumOfLeftLeaves(root.left);    // 左
+        int rightValue = sumOfLeftLeaves(root.right);  // 右
+                                                       
+        int midValue = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) { // 中
+            midValue = root.left.val;
+        }
+        int sum = midValue + leftValue + rightValue;
+        return sum;
+    }
+}
