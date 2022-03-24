96746eed256d2327bb6c5a8b9e25594f9d490ad7
LUKE-bit <57066589+LUKE-bit@users.noreply.github.com>
2022-03-24 11:19:59 +0800

    动态规划

---
+++ 不相交的线.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        //dp定义:dp[i][j]表示nums1 i-1之前,nums2 j-1之前最大连线数
+        //递推公式:dp[i][j],两种情况
+        //nums1[i] = nums2[j],dp[i][j] =Math.max(Math.max(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1
+        //不相等:dp[i][j] = Math.max(Math.max(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+        int[][] dp = new int[nums1.length+1][nums2.length+1];
+        for(int i = 1;i <= nums1.length;i++) {
+            for(int j = 1;j <= nums2.length;j++) {
+                if(nums1[i-1]==nums2[j-1]) {
+                    dp[i][j] =dp[i-1][j-1]+1;
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[nums1.length][nums2.length];
+    }
+}
